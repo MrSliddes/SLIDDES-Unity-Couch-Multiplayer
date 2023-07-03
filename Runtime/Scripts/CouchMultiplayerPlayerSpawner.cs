4b64564e99459d8aa2ce3189fdf436cb5514554a
MrSliddes <mrsliddes@outlook.com>
2023-07-03 22:37:19 +0200

    v1.0.3

--- Runtime/Scripts/CouchMultiplayerPlayerSpawner.cs
+++ Runtime/Scripts/CouchMultiplayerPlayerSpawner.cs
@@ -132,7 +132,7 @@ namespace SLIDDES.Multiplayer.Couch
             //GameObject a = Instantiate(prefabPlayer, spawn.position, spawn.rotation);            
             PlayerInput playerInput = PlayerInput.Instantiate(prefabPlayer, playerData.playerIndex, "", -1, pairWithDevice: playerData.inputDevice);
             GameObject a = playerInput.gameObject;
-            a.transform.SetParent(spawn);
+            a.transform.SetParent(spawn, false);
 
             // Get / create cmp
             CouchMultiplayerPlayerBase cmpb = a.GetComponent<CouchMultiplayerPlayerBase>();
